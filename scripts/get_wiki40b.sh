f1beedec36bf0b50109eb8061220413a5bb9e78e
Piotr Nawrot <p.nawrot99@gmail.com>
2022-11-18 13:31:46 +0000

    clean wiki

--- scripts/get_wiki40b.sh
+++ scripts/get_wiki40b.sh
@@ -1,11 +1,12 @@
 lang=$1
 splits="train validation test"
 
-python scripts/download_wiki40b.py $lang
+#python scripts/download_wiki40b.py $lang
 
-#for split in $splits 
-#do
-#    echo "./data/wiki40b/$lang/$split.txt"
-#        mkdir -p ./data/wiki40b/$lang/text8
-#    python cleaners/clean.py ./data/wiki40b/$lang/$split.txt $lang text8 > ./data/wiki40b/$lang/text8/$split.txt
-#done
+mkdir -p ./data/wiki40b/$lang/text8
+
+for split in $splits 
+do
+    echo "./data/wiki40b/$lang/$split.txt"
+    python cleaners/clean.py ./data/wiki40b/$lang/$split.txt $lang text8 > ./data/wiki40b/$lang/text8/$split.txt
+done
